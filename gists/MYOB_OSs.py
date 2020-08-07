54cf0c89fdd5f6686bf257014f570071981f53ec
Tim A <tabdiukov@gmail.com>
2020-08-07 13:37:46 +1000

    bugfix; better descriptiveness

--- gists/MYOB_OSs.py
+++ gists/MYOB_OSs.py
@@ -13,6 +13,8 @@ fp.close()
 r = re.compile(b'Operating System = (.*?)[\x00-\x10]')
 
 matches = r.findall(bs)
+
+print("OSs supported: ")
 matches_in_str = [buf.decode('ascii') for buf in matches]
 matches_in_str = sorted(matches_in_str)
-print(matches_in_str, sep='\n')
+[print(s, sep='\n') for s in matches_in_str]
