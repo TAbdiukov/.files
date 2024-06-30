328b9319255579698544318594d58b4394172b38
TAbdiukov <TAbdiukov@gmail.com>
2024-06-30 16:22:39 +0200

    CODE files cleanup

--- gists/midi2audio_wrap.py
+++ gists/midi2audio_wrap.py
@@ -8,19 +8,19 @@ def main():
 
 		print("USAGE: python "+PROGRAM_NAME+".py <file.mid(i)>  <target_format>")
 		print("For example: python "+PROGRAM_NAME+".py 1270.mid FLAC")
-		
+
 	else:
 		# get midi name
 		input = str(sys.argv[1])
 		target = str(sys.argv[2]).lower().split(".")[-1]
 		file = Path(input)
-				
+
 		fs = FluidSynth()
 		# Path of file for object copy (ambiguous behaviour in Python pathlib)
 		#print(file.resolve())
 		#print(Path(file).with_suffix("."+target).resolve())
-		
+
 		fs.midi_to_audio(file.resolve(), (Path(file).with_suffix("."+target).resolve()))
-		
+
 if __name__ == '__main__':
 	main()
